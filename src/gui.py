9c9f504fcb4c14652726a0fe59d0d93e1bbb011f
Lance Sparrow <lcsparrow@liberty.edu>
2024-03-12 15:58:03 -0400

    pieces loaded. next I need to handle keyboard events. not sure how to manipulate the pieces using the mouse.

--- src/gui.py
+++ src/gui.py
@@ -50,16 +50,89 @@ def drawBoard(board):
     return board
 
 def loadImg(board):
-    bBishop = pygame.image.load("chess/img/black-bishop.png")
-    bBishop = pygame.Surface.convert(bBishop)
-    pygame.Surface.blit(WINDOW, bBishop, (900,600))
+    global WINDOW
+
+    #load black pieces to board
+    bRook1 = pygame.image.load("chess/img/black-rook.png")
+    bRook1 = pygame.transform.scale(bRook1, (77.5, 77.5))
+    pygame.Surface.blit(WINDOW, bRook1, board[0][0])
+    bKnight1 = pygame.image.load("chess/img/black-knight.png")
+    bKnight1 = pygame.transform.scale(bKnight1, (77.5,77.5))
+    pygame.Surface.blit(WINDOW, bKnight1, board[0][1])
+    bBishop1 = pygame.image.load("chess/img/black-bishop.png")
+    bBishop1 = pygame.transform.scale(bBishop1, (77.5, 77.5))
+    pygame.Surface.blit(WINDOW, bBishop1, board[0][2])
+    bQueen = pygame.image.load("chess/img/black-queen.png")
+    bQueen = pygame.transform.scale(bQueen, (77.5, 77.5))
+    pygame.Surface.blit(WINDOW, bQueen, board[0][3])
     bKing = pygame.image.load("chess/img/black-king.png")
-    bKing = pygame.Surface.convert(bBishop)
+    bKing = pygame.transform.scale(bKing, (77.5,77.5))
+    pygame.Surface.blit(WINDOW, bKing, board[0][4])
+    bBishop2 = pygame.image.load("chess/img/black-bishop.png")
+    bBishop2 = pygame.transform.scale(bBishop2, (77.5, 77.5))
+    pygame.Surface.blit(WINDOW, bBishop2, board[0][5])
+    bKnight2 = pygame.image.load("chess/img/black-knight.png")
+    bKnight2 = pygame.transform.scale(bKnight2, (77.5,77.5))
+    pygame.Surface.blit(WINDOW, bKnight2, board[0][6])
+    bRook2 = pygame.image.load("chess/img/black-rook.png")
+    bRook2 = pygame.transform.scale(bRook2, (77.5, 77.5))
+    pygame.Surface.blit(WINDOW, bRook2, board[0][7])
+
+    #load white pieces to board
+    wRook1 = pygame.image.load("chess/img/white-rook.png")
+    wRook1 = pygame.transform.scale(wRook1, (77.5, 77.5))
+    pygame.Surface.blit(WINDOW, wRook1, board[7][0])
+    wKnight1 = pygame.image.load("chess/img/white-knight.png")
+    wKnight1 = pygame.transform.scale(wKnight1, (77.5,77.5))
+    pygame.Surface.blit(WINDOW, wKnight1, board[7][1])
+    wBishop1 = pygame.image.load("chess/img/white-bishop.png")
+    wBishop1 = pygame.transform.scale(wBishop1, (77.5, 77.5))
+    pygame.Surface.blit(WINDOW, wBishop1, board[7][2])
+    wQueen = pygame.image.load("chess/img/white-queen.png")
+    wQueen = pygame.transform.scale(wQueen, (77.5,77.5))
+    pygame.Surface.blit(WINDOW, wQueen, board[7][3])
+    wKing = pygame.image.load("chess/img/white-king.png")
+    wKing = pygame.transform.scale(wKing, (77.5,77.5))
+    pygame.Surface.blit(WINDOW, wKing, board[7][4])
+    wBishop2 = pygame.image.load("chess/img/white-bishop.png")
+    wBishop2 = pygame.transform.scale(wBishop2, (77.5,77.5))
+    pygame.Surface.blit(WINDOW, wBishop2, board[7][5])
+    wKnight2 = pygame.image.load("chess/img/white-knight.png")
+    wKnight2 = pygame.transform.scale(wKnight2, (77.5,77.5))
+    pygame.Surface.blit(WINDOW, wKnight2, board[7][6])
+    wRook2 = pygame.image.load("chess/img/white-rook.png")
+    wRook2 = pygame.transform.scale(wRook2, (77.5, 77.5))
+    pygame.Surface.blit(WINDOW, wRook2, board[7][7])
+
+    #create white and black pawn arrays for storing png
+    wPawn = pygame.image.load("chess/img/white-pawn.png")
+    wPawn = pygame.transform.scale(wPawn, (77.5,77.5))
+    wPawnArr = [wPawn] * 8
+
+    bPawn = pygame.image.load("chess/img/black-pawn.png")
+    bPawn = pygame.transform.scale(bPawn, (77.5,77.5))
+    bPawnArr = [bPawn] * 8
+
+    #load pawns into game
+    wPawnCount, bPawnCount, pawnCount = (0, 0, 0)
+    
+    while pawnCount < 16:
+        if wPawnCount != wPawnArr.__len__():
+            pygame.Surface.blit(WINDOW, wPawn, board[6][wPawnCount])
+            wPawnCount += 1
+            pawnCount += 1
+        elif bPawnCount != bPawnArr.__len__():
+            pygame.Surface.blit(WINDOW, bPawn, board[1][bPawnCount])
+            bPawnCount += 1
+            pawnCount += 1
+
+    
+    pygame.display.update()
 
 
 while RUNNING:
     for event in pygame.event.get():
-        if event.type ==pygame.QUIT:
+        if event.type == pygame.QUIT:
             RUNNING = False
     WINDOW.fill("black")
     
