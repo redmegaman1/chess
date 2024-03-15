c740de30bfda7e278d646a3acb7fc1ded7f717e5
Lance Sparrow <lcsparrow@liberty.edu>
2024-03-15 15:38:18 -0400

    working on board logic. i think i have currently mapped the images to the board in the array boardDraw and the underlying logic which will be used to move the pieces that are seen in boardLogic. I need to next add a way to capture the piece I have selected and move it to the square that I select next

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,4 @@
-#gui for chess board
+#gui for chess boardDraw 
 import pygame
 
 #global variables
@@ -11,6 +11,25 @@ DARK = (75, 0, 230)
 ROWS, COLS = (8, 8)
 Z = 0
 BOARDEXISTS = False
+BROOK1 = -5
+BROOK2 = -5
+BKNIGHT1 = -3
+BKNIGHT2 = -3
+BBISHOP1 = -3
+BBISHOP2 = -3
+BKING = -99
+BQUEEN = -9
+BPAWN = -1
+WROOK1 = 5
+WROOK2 = 5
+WKNIGHT1 = 3
+WKNIGHT2 = 3
+WBISHOP1 = 3
+WBISHOP2 = 3
+WKING = 99
+WQUEEN = 9
+WPAWN = 1
+EMPTY = 0
 
 #checks if a given integer is even or odd. returns true if odd and false if even
 def isOdd(int):
@@ -23,85 +42,85 @@ def isOdd(int):
 def initializeBoard():
     global Z, ROWS, COLS
 
-    board = [[0 for i in range(COLS)] for j in range(ROWS)]
+    boardDraw = [[0 for i in range(COLS)] for j in range(ROWS)]
     for a in range(ROWS):
         for b in range(COLS):
-            board[a][b] = Z
+            boardDraw[a][b] = Z
             Z += 1
-    return board
+    return boardDraw 
 
-#draws our board and maps eaach square to our 2d array. The 2d array is used for loading the img files and game logic
-def drawBoard(board):
+#draws our  boardDraw and maps eaach square to our 2d array. The 2d array is used for loading the img files and game logic
+def drawBoard(boardDraw):
     global WINDOW, LIGHT, DARK
 
     for a in range(COLS):
         for b in range(ROWS):
             if isOdd(a) == True and isOdd(b) == True and b < 8:
-                board[a][b] = pygame.draw.rect(WINDOW, LIGHT, pygame.Rect(b*77.5, a*77.5, 77.5, 77.5), 0)
+                 boardDraw[a][b] = pygame.draw.rect(WINDOW, LIGHT, pygame.Rect(b*77.5, a*77.5, 77.5, 77.5), 0)
             elif isOdd(a) == True and isOdd(b) == False and b < 8:
-                board[a][b] = pygame.draw.rect(WINDOW, DARK, pygame.Rect(b*77.5, a*77.5, 77.5, 77.5), 0)
+                 boardDraw[a][b] = pygame.draw.rect(WINDOW, DARK, pygame.Rect(b*77.5, a*77.5, 77.5, 77.5), 0)
             elif isOdd(a) == False and isOdd(b) == True and b < 8:
-                board[a][b] = pygame.draw.rect(WINDOW, DARK, pygame.Rect(b*77.5, a*77.5, 77.5, 77.5), 0)                
+                 boardDraw[a][b] = pygame.draw.rect(WINDOW, DARK, pygame.Rect(b*77.5, a*77.5, 77.5, 77.5), 0)                
             else:
-                board[a][b] = pygame.draw.rect(WINDOW, LIGHT, pygame.Rect(b*77.5, a*77.5, 77.5, 77.5), 0)
+                 boardDraw[a][b] = pygame.draw.rect(WINDOW, LIGHT, pygame.Rect(b*77.5, a*77.5, 77.5, 77.5), 0)
 
     pygame.display.flip()
-    return board
+    return boardDraw 
 
-def loadImg(board):
+def loadImg(boardDraw):
     global WINDOW
 
-    #load black pieces to board
+    #load black pieces to boardDraw 
     bRook1 = pygame.image.load("chess/img/black-rook.png")
     bRook1 = pygame.transform.scale(bRook1, (77.5, 77.5))
-    pygame.Surface.blit(WINDOW, bRook1, board[0][0])
+    pygame.Surface.blit(WINDOW, bRook1,  boardDraw[0][0])
     bKnight1 = pygame.image.load("chess/img/black-knight.png")
     bKnight1 = pygame.transform.scale(bKnight1, (77.5,77.5))
-    pygame.Surface.blit(WINDOW, bKnight1, board[0][1])
+    pygame.Surface.blit(WINDOW, bKnight1,  boardDraw[0][1])
     bBishop1 = pygame.image.load("chess/img/black-bishop.png")
     bBishop1 = pygame.transform.scale(bBishop1, (77.5, 77.5))
-    pygame.Surface.blit(WINDOW, bBishop1, board[0][2])
+    pygame.Surface.blit(WINDOW, bBishop1,  boardDraw[0][2])
     bQueen = pygame.image.load("chess/img/black-queen.png")
     bQueen = pygame.transform.scale(bQueen, (77.5, 77.5))
-    pygame.Surface.blit(WINDOW, bQueen, board[0][3])
+    pygame.Surface.blit(WINDOW, bQueen,  boardDraw[0][3])
     bKing = pygame.image.load("chess/img/black-king.png")
     bKing = pygame.transform.scale(bKing, (77.5,77.5))
-    pygame.Surface.blit(WINDOW, bKing, board[0][4])
+    pygame.Surface.blit(WINDOW, bKing,  boardDraw[0][4])
     bBishop2 = pygame.image.load("chess/img/black-bishop.png")
     bBishop2 = pygame.transform.scale(bBishop2, (77.5, 77.5))
-    pygame.Surface.blit(WINDOW, bBishop2, board[0][5])
+    pygame.Surface.blit(WINDOW, bBishop2,  boardDraw[0][5])
     bKnight2 = pygame.image.load("chess/img/black-knight.png")
     bKnight2 = pygame.transform.scale(bKnight2, (77.5,77.5))
-    pygame.Surface.blit(WINDOW, bKnight2, board[0][6])
+    pygame.Surface.blit(WINDOW, bKnight2,  boardDraw[0][6])
     bRook2 = pygame.image.load("chess/img/black-rook.png")
     bRook2 = pygame.transform.scale(bRook2, (77.5, 77.5))
-    pygame.Surface.blit(WINDOW, bRook2, board[0][7])
+    pygame.Surface.blit(WINDOW, bRook2,  boardDraw[0][7])
 
-    #load white pieces to board
+    #load white pieces to boardDraw 
     wRook1 = pygame.image.load("chess/img/white-rook.png")
     wRook1 = pygame.transform.scale(wRook1, (77.5, 77.5))
-    pygame.Surface.blit(WINDOW, wRook1, board[7][0])
+    pygame.Surface.blit(WINDOW, wRook1,  boardDraw[7][0])
     wKnight1 = pygame.image.load("chess/img/white-knight.png")
     wKnight1 = pygame.transform.scale(wKnight1, (77.5,77.5))
-    pygame.Surface.blit(WINDOW, wKnight1, board[7][1])
+    pygame.Surface.blit(WINDOW, wKnight1,  boardDraw[7][1])
     wBishop1 = pygame.image.load("chess/img/white-bishop.png")
     wBishop1 = pygame.transform.scale(wBishop1, (77.5, 77.5))
-    pygame.Surface.blit(WINDOW, wBishop1, board[7][2])
+    pygame.Surface.blit(WINDOW, wBishop1,  boardDraw[7][2])
     wQueen = pygame.image.load("chess/img/white-queen.png")
     wQueen = pygame.transform.scale(wQueen, (77.5,77.5))
-    pygame.Surface.blit(WINDOW, wQueen, board[7][3])
+    pygame.Surface.blit(WINDOW, wQueen,  boardDraw[7][3])
     wKing = pygame.image.load("chess/img/white-king.png")
     wKing = pygame.transform.scale(wKing, (77.5,77.5))
-    pygame.Surface.blit(WINDOW, wKing, board[7][4])
+    pygame.Surface.blit(WINDOW, wKing,  boardDraw[7][4])
     wBishop2 = pygame.image.load("chess/img/white-bishop.png")
     wBishop2 = pygame.transform.scale(wBishop2, (77.5,77.5))
-    pygame.Surface.blit(WINDOW, wBishop2, board[7][5])
+    pygame.Surface.blit(WINDOW, wBishop2,  boardDraw[7][5])
     wKnight2 = pygame.image.load("chess/img/white-knight.png")
     wKnight2 = pygame.transform.scale(wKnight2, (77.5,77.5))
-    pygame.Surface.blit(WINDOW, wKnight2, board[7][6])
+    pygame.Surface.blit(WINDOW, wKnight2,  boardDraw[7][6])
     wRook2 = pygame.image.load("chess/img/white-rook.png")
     wRook2 = pygame.transform.scale(wRook2, (77.5, 77.5))
-    pygame.Surface.blit(WINDOW, wRook2, board[7][7])
+    pygame.Surface.blit(WINDOW, wRook2,  boardDraw[7][7])
 
     #create white and black pawn arrays for storing png
     wPawn = pygame.image.load("chess/img/white-pawn.png")
@@ -117,35 +136,94 @@ def loadImg(board):
     
     while pawnCount < 16:
         if wPawnCount != wPawnArr.__len__():
-            pygame.Surface.blit(WINDOW, wPawn, board[6][wPawnCount])
+            pygame.Surface.blit(WINDOW, wPawn, boardDraw[6][wPawnCount])
             wPawnCount += 1
             pawnCount += 1
         elif bPawnCount != bPawnArr.__len__():
-            pygame.Surface.blit(WINDOW, bPawn, board[1][bPawnCount])
+            pygame.Surface.blit(WINDOW, bPawn, boardDraw[1][bPawnCount])
             bPawnCount += 1
             pawnCount += 1
-
     
     pygame.display.update()
 
+def logicalBoard(boardDraw):
+    global BROOK1, BROOK2, BBISHOP1, BBISHOP2, BKNIGHT1, BKNIGHT2, BQUEEN, BKING, BPAWN, WROOK1, WROOK2, WBISHOP1, WBISHOP2, WKNIGHT1, WKNIGHT2, WQUEEN, WKING, WPAWN, EMPTY, ROWS
 
-while RUNNING:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUNNING = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and RUNNING == True:
-            x, y = pygame.mouse.get_pos()
-    WINDOW.fill("black")
-    
-    #place game here. need to draw a board, fill it in, and place pieces.
-    #functionalitY of game should be able to be contained in functions contained elsewhere.
-    if BOARDEXISTS == True:
-        continue
-    else:
-        board = initializeBoard()
-        drawBoard(board)
-        loadImg(board)
-        BOARDEXISTS = True
+    boardLogic = boardDraw
 
-    CLOCK.tick(60)
-pygame.quit()
+    boardLogic[0][0] = BROOK1
+    boardLogic[0][1] = BKNIGHT1
+    boardLogic[0][2] = BBISHOP1
+    boardLogic[0][3] = BQUEEN
+    boardLogic[0][4] = BKING
+    boardLogic[0][5] = BBISHOP2
+    boardLogic[0][6] = BKNIGHT2
+    boardLogic[0][7] = BROOK2
+    for rows in range(ROWS):
+        boardLogic[1][rows] = BPAWN
+
+    i = (0, 1, 2, 3)
+    j = (0, 1,2,3,4,5,6,7)
+    for x in i:
+        for y in j:
+            boardLogic[y][x] = EMPTY
+
+    boardLogic[7][0] = WROOK1
+    boardLogic[7][1] = WKNIGHT1
+    boardLogic[7][2] = WBISHOP1
+    boardLogic[7][3] = WQUEEN
+    boardLogic[7][4] = WKING
+    boardLogic[7][5] = WBISHOP2
+    boardLogic[7][6] = WKNIGHT2
+    boardLogic[7][7] = WROOK2
+    for rows in range(ROWS):
+        boardLogic[6][rows] = WPAWN
+    return boardLogic
+
+def selectSquare(x, y):
+    global ROWS, COLS
+    for col in range(COLS):
+        for row in range(ROWS):
+            if x > row*77 and x < (row+1)*77 and y > col *77 and y < (col+1)*77:
+                return (x, y)
+            else:
+                continue
+
+#def movePiece(square, boardLogic):
+
+
+def main():
+    global RUNNING, WINDOW, BOARDEXISTS, CLOCK
+    selectFlag = False
+
+    while RUNNING:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUNNING = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and RUNNING == True and selectFlag == False:
+                x, y = pygame.mouse.get_pos()
+                square = selectSquare(x, y)
+                selectFlag = True
+            elif event.type == pygame.MOUSEBUTTONDOWN and RUNNING == True and selectFlag == True:
+                movePiece(square, boardLogic)
+                selectFlag = False
+                
+
+        WINDOW.fill("black")
+        
+        #place game here. need to draw a  boardDraw, fill it in, and place pieces.
+        #functionalitY of game should be able to be contained in functions contained elsewhere.
+        if BOARDEXISTS == True:
+            continue
+        else:
+            boardDraw = initializeBoard()
+            boardLogic = logicalBoard(boardDraw)
+            drawBoard(boardDraw)
+            loadImg(boardDraw)
+            BOARDEXISTS = True
+
+        CLOCK.tick(60)
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
